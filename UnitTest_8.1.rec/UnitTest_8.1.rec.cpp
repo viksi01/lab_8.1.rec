52e4ce941a9914d804c1f044c29a43643a38c5cb
viksi01 <victoria.pyatnicka@gmail.com>
2022-12-05 01:06:54 +0200

    commit1_8.1.rec

---
+++ UnitTest_8.1.rec/UnitTest_8.1.rec.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1.rec/lab_8.1.rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rec
+{
+	TEST_CLASS(UnitTest81rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* str = "abcooobbaoooocab";
+			int count = Count(str, 0);
+			Assert::AreEqual(3, count);
+
+		}
+	};
+}
---
+++ lab_8.1.rec/lab_8.1.rec.cpp
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int Count(char* str, int i)
+{
+	if (strlen(str) < 3)
+		return 0;
+	
+		if (str[i + 2] != 0)
+			if ((str[i] == 'a' || str[i] == 'b' || str[i] == 'c') && (str[i + 1] == 'a' || str[i + 1] == 'b' || str[i + 1] == 'c')
+				&& (str[i + 2] == 'a' || str[i + 2] == 'b' || str[i + 2] == 'c'))
+				return 1 + Count(str, i + 1);
+			else
+				return Count(str, i + 1);
+		else
+			return 0;
+}
+
+char* Change(char* dest, const char* str, char* t, int i)
+{
+	if (str[i + 2] != 0)
+	{
+		if ((str[i] == 'a' || str[i] == 'b' || str[i] == 'c') && (str[i + 1] == 'a' || str[i + 1] == 'b' || str[i + 1] == 'c')
+			&& (str[i + 2] == 'a' || str[i + 2] == 'b' || str[i + 2] == 'c'))
+		{
+			strcat(t, "**");
+			return Change(dest, str, t + 2, i + 3);
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+			return Change(dest, str, t, i);
+		}
+	}
+	else
+	{
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t = '\0';
+		return dest;
+	}
+}
+
+int main()
+{
+	char str[101];
+	int i = 0;
+	cout << "Enter string:";  cin.getline(str, 100);
+	cout << "String contained " << Count(str, i) << " groups of 'a' ,'b', 'c'" << endl;
+	char* dest1 = new char[151];
+	dest1[0] = '\0';
+	char* dest2;
+	dest2 = Change(dest1, str, dest1, 0);
+	cout << "Modified string (param) : " << dest1 << endl;
+	cout << "Modified string (result): " << dest2 << endl;
+	return 0;
+
+}
